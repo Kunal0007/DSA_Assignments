e5a8d5f7134a15b6c0ab0a9a0be8e13be418e50c
Kunal0007 <kunal.kv1dr64@gmail.com>
2021-10-15 12:11:05 +0530

    Greeting to Input

---
+++ Greeting.java
@@ -0,0 +1,13 @@
+package com.company.DSA_Assignments;
+
+import java.util.Scanner;
+
+// Take name as input and print a greeting message for that name.
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner in  = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.println("Hello " + name + ", Welcome to My Github Repo.");
+    }
+}
